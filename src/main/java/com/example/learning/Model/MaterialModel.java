b5aebf6bce95d3b16f1de65482da8cc611486944
Satyamray21 <satyamray43@gmail.com>
2023-11-28 19:40:35 +0530

    c

--- src/main/java/com/example/learning/Model/MaterialModel.java
+++ src/main/java/com/example/learning/Model/MaterialModel.java
@@ -18,7 +18,7 @@ public class MaterialModel {
     String content;
     @ManyToOne
     @JoinColumn(name="user_id")
-    
+    UserModel user;
     public Long getId() {
         return id;
     }
@@ -37,6 +37,12 @@ public class MaterialModel {
     public void setContent(String content) {
         this.content = content;
     }
+    public UserModel getUser() {
+        return user;
+    }
+    public void setUser(UserModel user) {
+        this.user = user;
+    }
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -44,6 +50,7 @@ public class MaterialModel {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((title == null) ? 0 : title.hashCode());
         result = prime * result + ((content == null) ? 0 : content.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
         return result;
     }
     @Override
@@ -70,12 +77,16 @@ public class MaterialModel {
                 return false;
         } else if (!content.equals(other.content))
             return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
         return true;
     }
     @Override
     public String toString() {
-        return "MaterialModel [id=" + id + ", title=" + title + ", content=" + content + "]";
+        return "MaterialModel [id=" + id + ", title=" + title + ", content=" + content + ", user=" + user + "]";
     }
-    
     
 }
